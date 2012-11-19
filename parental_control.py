58cbca8b5102dee69f7ca2237365b6f1fde00646
Ilya Kaminsky <ilyakam@gmail.com>
2012-11-19 01:22:06 -0800

    Cleaned up some variables. Added more comments for clarity.

--- parental_control.py
+++ parental_control.py
@@ -3,18 +3,11 @@ import sublime, sublime_plugin
 # Removes parentheses around the cursor in a given line:
 class RemoveParenthesesCommand(sublime_plugin.TextCommand):
   parentheses_types = ["(", "[", "{"]
-  parentheses_match = {"(":")",
-                       "[":"]",
-                       "{":"}"}
+  parentheses_match = {"(": ")",
+                       "[": "]",
+                       "{": "}"}
   
   def run(self, edit):
-    # TODO:
-    # - Clean up variable names:
-    #   - Use position hash
-    #   - Rename all brackets to parentheses
-    # - Reorganize code blocks with defined functions
-    # - Try to be more DRY
-    
     view       = self.view
     selections = view.sel() # the cursor position in each multiline selection
     
@@ -23,49 +16,48 @@ class RemoveParenthesesCommand(sublime_plugin.TextCommand):
       if not selection.empty(): continue
       
       seeking_position = selection.begin()
-      end_of_file      = view.size()
-      
-      left_bracket        = False
-      right_bracket       = False
-      closing_parentheses = False
+      opening_position = False
+      closing_position = False
+      closing_matching = False # the matching closing parentheses
       
       # Look backwards until you find a parentheses:
       while seeking_position > 0:
         seeking_position -= 1
         point             = view.substr(seeking_position)
         
+        # If opening parentheses found
         if point in self.parentheses_types:
-          closing_parentheses = self.parentheses_match[point]
-          left_bracket = seeking_position
+          closing_matching = self.parentheses_match[point]
+          opening_position = seeking_position
           break
       
       # Stop processing this selection if no opening parentheses found:
-      if not closing_parentheses or not left_bracket: continue
+      if not closing_matching or not opening_position: continue
       
       # Reinitialize the seeking position:
       seeking_position = selection.begin()
       
-      # Stop processing this selection if we're outside the matching parenthesis:
-      while seeking_position > left_bracket:
+      # Stop processing this selection if we're outside the matching parentheses:
+      while seeking_position > opening_position:
         seeking_position -= 1
-        if view.substr(seeking_position) == closing_parentheses:
-          left_bracket = False
+        if view.substr(seeking_position) == closing_matching:
+          opening_position = False
           break
         
       # Reinitialize the seeking position:
       seeking_position = selection.begin()
       
-      # Look forward until you find a parentheses:
-      while closing_parentheses and seeking_position < end_of_file:
+      # Look forward through EOF until you find a matching closing parentheses:
+      while closing_matching and seeking_position < view.size():
         point = view.substr(seeking_position)
         
-        if point == closing_parentheses:
-          right_bracket = seeking_position
+        if point == closing_matching:
+          closing_position = seeking_position
           break
         
         seeking_position += 1
       
-      # Actually replace the brackets with a space and an empty character:
-      if left_bracket and right_bracket:
-        view.replace(edit, sublime.Region(left_bracket, left_bracket + 1),  " ")
-        view.replace(edit, sublime.Region(right_bracket, right_bracket + 1), "")
+      # Actually replace the parentheses pair with a space and an empty character:
+      if opening_position and closing_position:
+        view.replace(edit, sublime.Region(opening_position, opening_position + 1), " ")
+        view.replace(edit, sublime.Region(closing_position, closing_position + 1), "")
